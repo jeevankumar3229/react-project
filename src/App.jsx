d1c2601581a58f830e9e6b116b667f9aa85c3e27
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-10-20 16:10:37 +0530

    code reading data from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>
-      APP
-    </div>
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+import NotFoundPage from "./Pages/NotFoundPage";
+import JobsPage from "./Pages/JobsPage";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import MainLayout from "./Layout/MainLayout";
+const route = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
   )
-}
+);
+const App = () => {
+  return <RouterProvider router={route} />;
+};
 
-export default App
+export default App;
